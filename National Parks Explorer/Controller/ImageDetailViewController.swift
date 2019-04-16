62cc000faa6588301c6e8783b17b2312ab83663d
Sighmox <Sighmox@github.com>
2019-04-16 11:40:38 -0500

    Completed the basic features of the app

--- National Parks Explorer/Controller/ImageDetailViewController.swift
+++ National Parks Explorer/Controller/ImageDetailViewController.swift
@@ -13,9 +13,27 @@ class ImageDetailViewController: UIViewController {
     var flickrImage: FlickrImage?
     var flickrService = FlickrService()
     
+    @IBOutlet var photoDetails: UILabel!
+    @IBOutlet var imageView: UIImageView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let photoData = flickrImage!.photoData
+        photoDetails.text = photoData!.title
+        let url = flickrImage!.fullURL
+        
+        flickrService.downloadImage(url: url!) { (image: UIImage?, error: Error?) -> Void in
+            DispatchQueue.main.async {
+                if let error = error {
+                    print(error)
+                    self.present(ErrorAlertController.alert(message: "Error fetching photo"), animated: true)
+                }
+                else {
+                    self.imageView.image = image
+                }
+            }
+        }
     }
 }
--- National Parks Explorer/FlickrImage.swift
+++ National Parks Explorer/FlickrImage.swift
@@ -22,7 +22,7 @@ init(photoData: FlickrPhotoData) {
 
     var thumbnailURL: String? {
         guard let photo = photoData else { return nil }
-        return "https//farm\(photo.farm).staticflickr.com/\(photo.server)/\(photo.id)_\(photo.secret)_q.jpg"
+        return "https://farm\(photo.farm).staticflickr.com/\(photo.server)/\(photo.id)_\(photo.secret)_q.jpg"
     }
     
     var fullURL: String? {
